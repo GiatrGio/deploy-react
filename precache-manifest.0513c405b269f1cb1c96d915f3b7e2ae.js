8883b618b664a878386bbe57839beec77961a7d3
Giatrakis <georgios.giatrakis@wur.nl>
2020-06-11 10:48:39 +0200

    Updates

---
+++ precache-manifest.0513c405b269f1cb1c96d915f3b7e2ae.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3381aa9caf521f14e344d03298769156",
+    "url": "/deploy-react/index.html"
+  },
+  {
+    "revision": "640cac16623bfd72fa9d",
+    "url": "/deploy-react/static/css/2.59640612.chunk.css"
+  },
+  {
+    "revision": "65b5549dcf436f25f9bf",
+    "url": "/deploy-react/static/css/main.33449fee.chunk.css"
+  },
+  {
+    "revision": "640cac16623bfd72fa9d",
+    "url": "/deploy-react/static/js/2.7a4e94b3.chunk.js"
+  },
+  {
+    "revision": "56729d980da09961afe8d497a21eec12",
+    "url": "/deploy-react/static/js/2.7a4e94b3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "65b5549dcf436f25f9bf",
+    "url": "/deploy-react/static/js/main.d3c73dbc.chunk.js"
+  },
+  {
+    "revision": "285caa80e43ffbcf60e3",
+    "url": "/deploy-react/static/js/runtime-main.1d20c137.js"
+  }
+]);
